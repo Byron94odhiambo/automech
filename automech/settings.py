a62736ad80c95f9416fa9388f62a9c90da6d28b1
Byron94odhiambo <odhiambobyron39@gmail.com>
2024-08-22 16:36:18 +0300

    Fixed database configuration

--- automech/settings.py
+++ automech/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'automech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
